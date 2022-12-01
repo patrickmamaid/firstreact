10f4bf528115a3ddd894a7ed5838f2552b4276ed
Patrick Mamaid <patrickmamaid@gmail.com>
2022-12-01 02:06:51 -0800

    game completed

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -50,6 +50,12 @@ class Board extends React.Component {
 
         const squares = this.state.squares.slice(); // basically copy the array from this states squares to here per handleclick
         // interesting how you can modify an array here ..
+
+        if (calculateWinner(squares) || squares[i] ) {
+            return;
+        }
+
+
         squares[i] = this.state.xIsNext ? 'X' : 'O'; // very ineteresting if statement
         this.setState({
             squares: squares,
@@ -80,7 +86,14 @@ class Board extends React.Component {
     // each section is calling its local renderSquare within this class passing an index value here
     // renderSquare returns
     render() {
-        const status = 'Next player: X';
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        }else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
 
         return (
             <div>
@@ -120,6 +133,30 @@ class Game extends React.Component {
         );
     }
 }
+
+
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
+
 
 // ========================================
 
