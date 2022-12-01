ed8c3f834341b23007f3790bfa0b97bad8002a78
Patrick Mamaid <patrickmamaid@gmail.com>
2022-12-01 00:18:07 -0800

    shows the use of props here

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 
-
+// {this.props.value}  called on by the board class
 class Square extends React.Component {
     render() {
         return (
             <button className="square">
-                {/* TODO */}
+                {this.props.value}
             </button>
         );
     }
@@ -16,7 +16,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
     renderSquare(i) {
-        return <Square />;
+        return <Square value={i} />; // passing prop called value here
     }
 
     render() {
