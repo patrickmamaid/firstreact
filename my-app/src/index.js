d8e679a38d5350c022807ff9e71afb635eaf75b1
Patrick Mamaid <patrickmamaid@gmail.com>
2022-12-02 00:17:09 -0800

    the craziness is done

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -26,6 +26,7 @@ import './index.css';
 
 // lets rewrite the square class above as a function
 function Square(props) {
+
     return(
         <button className="square" onClick={props.onClick}>
             {props.value}
@@ -37,46 +38,15 @@ function Square(props) {
 
 class Board extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null), // fill up 9 boxes but empty
-            xIsNext: true,
-        }
-    }
 
 
-    handleClick(i){
 
-        const squares = this.state.squares.slice(); // basically copy the array from this states squares to here per handleclick
-        // interesting how you can modify an array here ..
-
-        if (calculateWinner(squares) || squares[i] ) {
-            return;
-        }
-
-
-        squares[i] = this.state.xIsNext ? 'X' : 'O'; // very ineteresting if statement
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
-        }); // omg because no pointers lol, once you copy it here, add X to an index and send it back to setstate lol wow
-
-
-        // so i have found that you should keep things immutable by doing copies apparently
-        // this is especially good when you want to do historic jumps back in time
-        // avoiding direct data mutation lets us keep pervious versions of the games history intact
-        // and reusing them later VERY COOL
-        // https://reactjs.org/tutorial/tutorial.html#setup-for-the-tutorial read DATA CHANGE WITH MUTATION
-        
-
-    }
 
     renderSquare(i) { // gets index 0
         return(
             <Square
-                value={this.state.squares[i]}
-                onClick={()=> this.handleClick(i) }
+                value={this.props.squares[i]}
+                onClick={()=> this.props.onClick(i) }
             /> // tell the square how it is by sending the square this value from our constructor/// by using the same index from the other render area
         );
     }
@@ -86,18 +56,8 @@ class Board extends React.Component {
     // each section is calling its local renderSquare within this class passing an index value here
     // renderSquare returns
     render() {
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        if (winner) {
-            status = 'Winner: ' + winner;
-        }else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-
-
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -119,15 +79,103 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            xIsNext: true,
+            stepNumber: 0,
+        };
+    }
+
+
+    handleClick(i){
+
+        //We will also replace reading this.state.history with this.state.history.slice(0, this.state.stepNumber + 1). This ensures that if we “go back in time” and then make a new move from that point, we throw away all the “future” history that would now be incorrect.
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();// basically copy the array from this states squares to here per handleclick
+        // interesting how you can modify an array here ..
+
+
+        if (calculateWinner(squares) || squares[i] ) {
+            return;
+        }
+
+
+        squares[i] = this.state.xIsNext ? 'X' : 'O'; // very ineteresting if statement
+        this.setState({
+            history: history.concat([{ //Unlike the array push() method you might be more familiar with, the concat() method doesn’t mutate the original array, so we prefer it.
+                squares: squares
+            }]),
+            xIsNext: !this.state.xIsNext,
+            stepNumber : history.length, // sets the step number to the top of the history when we handle the click here
+        }); // omg because no pointers lol, once you copy it here, add X to an index and send it back to setstate lol wow
+
+
+        // so i have found that you should keep things immutable by doing copies apparently
+        // this is especially good when you want to do historic jumps back in time
+        // avoiding direct data mutation lets us keep pervious versions of the games history intact
+        // and reusing them later VERY COOL
+        // https://reactjs.org/tutorial/tutorial.html#setup-for-the-tutorial read DATA CHANGE WITH MUTATION
+
+
+    }
+
+
+// Notice in jumpTo method, we haven’t updated history property of the state. That is because state updates are merged or in more simple words React will update only the properties mentioned in setState method leaving the remaining state as is. For more info see the documentation.
+    // https://reactjs.org/docs/state-and-lifecycle.html#state-updates-are-merged
+    jumpTo(step){
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        })
+    }
+
+
+
+
+
     render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
+
+
+        // map = function(element , index, array) { return 'whateveryouwant' // or array }
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Go to move #' + move :
+                'Go to game start';
+            return(
+                <li key={move}>
+                    <button onClick={()=> this.jumpTo(move)} >{desc}</button>
+                </li>
+            );
+        });
+
+
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+
+        }else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' :'O');
+        }
+
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board
+                        squares={current.squares}
+                        onClick={(i)=> this.handleClick(i)}
+                    />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
